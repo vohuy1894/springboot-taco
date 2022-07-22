8caa2f08a60489bad006755b287aafd9122c1b9e
Huy Vo <vohuy1894@gmail.com>
2022-07-22 17:08:31 -0500

    Initial commit

---
+++ src/main/java/com/example/taco/SpringbootTacoApplication.java
@@ -0,0 +1,13 @@
+package com.example.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taco/SpringbootTacoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.taco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringbootTacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
