8d7e0821189651a78e733dc7db11c594836d93b0
Huy Vo <vohuy1894@gmail.com>
2022-07-22 17:15:54 -0500

    add ingredient class

--- src/main/java/com/example/taco/HomeController.java
+++ src/main/java/com/example/taco/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public @ResponseBody String greeting() {
-        return "Hello, World";
+    public String home() {
+        return "home";
     }
 }
---
+++ src/main/java/com/example/taco/Ingredient.java
@@ -0,0 +1,15 @@
+package com.example.taco;
+
+import lombok.Data;
+
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type{
+        WRAP, PROTEIN, VEGGIES, CHESSE, SAUCE
+    }
+}
